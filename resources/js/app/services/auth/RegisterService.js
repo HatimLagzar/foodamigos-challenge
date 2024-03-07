cc61477f95ff5f3278769d6434bb4ac3c5b73936
Hatim Lagzar <lhatimdev@gmail.com>
2024-03-07 21:03:00 +0100

    Register js service

---
+++ resources/js/app/services/auth/RegisterService.js
@@ -0,0 +1,27 @@
+import { register } from '../../api/auth/registerApi';
+import toastr from 'toastr';
+
+class RegisterService {
+  register(name, phoneNumber, password) {
+    const formData = new FormData();
+    formData.set('name', name);
+    formData.set('phone_number', phoneNumber);
+    formData.set('password', password);
+
+    return register(formData).catch((error) => {
+      if (error.response) {
+        if (error.response.status === 422) {
+          toastr.error(error.response.data.message);
+
+          return error;
+        }
+      }
+
+      console.error(error);
+
+      throw error;
+    });
+  }
+}
+
+export default new RegisterService();
